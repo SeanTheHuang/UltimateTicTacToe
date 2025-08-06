b4bca1b6fcd57e979952c28e743912c8f6a7bacf
SeanTheHuang <sean.the.huang@gmail.com>
2025-08-06 20:54:52 +0100

    More visuals stuff

---
+++ Assets/Scripts/Game/BoardVisualsManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Game
+{ 
+    public class BoardVisualsManager : MonoBehaviour
+    {
+        public GameObject m_p1Prefab = null;
+        public GameObject m_p2Prefab = null;
+
+
+    } // public class BoardVisualsManager
+
+} // namespace Game
--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -3,15 +3,17 @@ using UnityEngine;
 
 namespace Game
 {
+    [RequireComponent(typeof(BoardVisualsManager))]
     public class GameManager : MonoBehaviour
     {
+        private BoardVisualsManager m_boardVisuals = null;
         private BoardManager m_board = new BoardManager();
         private BoardState m_activePlayer = BoardState.None;
         private int? m_activeBoardIndex = null; // null means player can place tile in any board
 
         public void Awake()
         {
-
+            m_boardVisuals = GetComponent<BoardVisualsManager>();
         }
 
         public void Start()
