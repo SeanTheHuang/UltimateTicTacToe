ac279e81717eb4233432affcd880795d368c7cb9
SeanTheHuang <sean.the.huang@gmail.com>
2025-08-07 21:57:51 +0100

    Pretty much nothing today :|

---
+++ Assets/Scripts/Game/BoardVisualTarget.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class BoardVisualTarget : MonoBehaviour
+{
+    public int? GetTileIndexAtLocation( Vector3 location )
+    {
+        // Do 2D check, only caring about x-z coords
+        // return tile index
+        // else return null
+
+        return null;
+    }
+}
